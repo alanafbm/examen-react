975fde2c831880750217e43e8a28d7dd2a035f48
alanamoraes <alanaaaa@hotmail.com>
2022-12-13 16:32:31 -0500

    fetch id

--- src/Xkcd/Xkcd.js
+++ src/Xkcd/Xkcd.js
@@ -4,10 +4,13 @@ import React from 'react';
 export default class Xkcd extends React.Component{
   constructor(props){
     super(props);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       bd: {},
       id: '',
       bdAvecID: {},
+      random: null,
+      
     }
   }
   componentDidMount(){
@@ -26,21 +29,33 @@ export default class Xkcd extends React.Component{
     })
 
 
-    fetch('https://cors.jonathanmartel.info/?url=https://xkcd.com/6/info.0.json')
+
+    
+
+
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    // console.log('random');
+    const min = 1;
+    const max = 100;
+    const random = Math.floor(Math.random() * 50); 
+    //this.setState({ random });
+    fetch(`https://cors.jonathanmartel.info/?url=https://xkcd.com/${random}/info.0.json`)
     .then((response) => {
       // console.log(response);
       if(response.ok) return response.json();
       else throw Error();
     })
     .then((data) => {
-      // console.log(data);
-      this.setState({ bdAvecID: data })
+      // bd(data);
+      console.log(data);
+      // console.log(this.state.bdAvecID);
     })
     .catch((err) => {
       console.log(err.message);
     })
-
-
   }
 
   changeID= (e) =>{
@@ -52,11 +67,11 @@ export default class Xkcd extends React.Component{
     e.preventDefault();
     //place pour faire la validation
     if(this.state.id !== ''){
-      // console.log(this.state.id);
       this.state.bd.num = this.state.id;
-      this.props.handleAfficheBD(this.state.id);
+      console.log(this.state.bd.num);
+
     }else{
-      // console.log(this.state.bd);
+      console.log(this.state.bd);
       this.state.bd.num = this.state.bd.num;
     }
   }
@@ -70,6 +85,7 @@ export default class Xkcd extends React.Component{
          <form>
             <input onBlur={this.changeID} type="text"></input>
             <button onClick={this.afficheBD}>Afficher BD</button>
+            <button onClick={this.handleClick}>Afficher BD aleatoire</button>
           </form>
           <div>
             <h2>{bd.title}</h2>
